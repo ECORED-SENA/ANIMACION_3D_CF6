744dad0487619694d1bed80ee82cf360511910a4
Edward Leonardo Pico Cabra <epicocabra@gmail.com>
2021-11-18 14:06:01 -0500

    Update Introduccion.vue

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -16,7 +16,7 @@
             p(data-aos="fade-down") Seleccionar las técnicas y herramientas de modelado, requiere de una serie de conocimientos frente a los elementos y la composición para la creación de productos a partir de la manipulación y combinación de elementos digitales, con el fin de generar objetos que den cuenta del proceso; aprendizajes que serán abordados en el presente componente formativo, como se ve a continuación: 
             figure.mt-5
               .video
-                iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+                iframe(width="560" height="315" src="https://www.youtube.com/embed/Md0VBuw9MBg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 
 </template>
